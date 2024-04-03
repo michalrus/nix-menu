3666b25e200a8709265aa8a672078f2880daaaca
Michal Rus <michal.rus@iohk.io>
2024-04-03 19:46:03 +0200

    Read the initial `menu_items` from a JSON file

--- menu.sh
+++ menu.sh
@@ -2,27 +2,27 @@
 
 set -euo pipefail
 
+# TODO: check for: dialog/whiptail, jq etc.
+
+# TODO: check that "$1" is set or fail with usage
+
+mapfile -d $'\0' menu_items < <(
+    jq -j '
+       sort_by(.subcommands | length > 0 | not)
+       | (map(.title | length) | max) as $max_length_alt
+       | .[]
+       | "\u0000",
+         .title,
+         (" " * ($max_length_alt - (.title | length))),
+         if .subcommands | length > 0 then "  » " else "    " end,
+         "\u0000",
+         .description
+    ' example.json | cut -c2-
+)
+
 exec dialog \
     --backtitle "nix-menu 0.5.4" \
-    --colors --keep-tite --ok-label "Select" --cancel-label "Back" --scrollbar \
+    --colors --keep-tite --ok-label "Select" --cancel-label "Back" --scrollbar --tab-correct \
     --title "Menu example" \
-    --menu "$(printf "Choose an option\n\nand a \Zbmulti-line string\Zn!\n")" 25 78 16 \
-    "Add User" "Add a user to the system." \
-    "Modify User" "Modify an existing user." \
-    "List Users" "List all users on the system." \
-    "Add Group" "Add a user group to the system." \
-    "Modify Group" "Modify a group and its list of members." \
-    "List Groups" "List all groups on the system." \
-    "Add User" "Add a user to the system." \
-    "Modify User" "Modify an existing user." \
-    "List Users" "List all users on the system." \
-    "Add Group" "Add a user group to the system." \
-    "Modify Group" "Modify a group and its list of members." \
-    "List Groups" "List all groups on the system." \
-    "Add User" "Add a user to the system." \
-    "Modify User" "Modify an existing user." \
-    "List Users" "List all users on the system." \
-    "Add Group" "Add a user group to the system." \
-    "Modify Group" "Modify a group and its list of members." \
-    "List Groups" "List all groups on the system." \
-    "Zażółć" "Zażółć gęślą jaźń."
+    --menu "$(printf "Choose an option\n\nand a \Zbmulti-line string\Zn!\n")" 0 0 0 \
+    "${menu_items[@]}"
