bd1882c98f406d81d4b96eb81261f36d2ac6545d
Michal Rus <m@michalrus.com>
2024-04-04 03:37:00 +0200

    Support `whiptail`, add proper `packages`, and `lib`

--- flake.nix
+++ flake.nix
@@ -9,9 +9,21 @@
       eachSystem = f: lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system: f system inputs.nixpkgs.legacyPackages.${system});
     in
     {
+      packages = eachSystem (system: pkgs: rec {
+        default = menu;
+        menu = inputs.self.lib.mkPackage { inherit pkgs; };
+        menu-example = inputs.self.lib.mkMenu {
+          inherit pkgs; name = "menu-example";
+          definition = builtins.fromJSON (builtins.readFile ./example.json);
+        };
+      });
+
       devShells = eachSystem (system: pkgs: {
         default = pkgs.mkShell {
-          buildInputs = (with pkgs; [
+          buildInputs = (with inputs.self.packages.${system}; [
+            menu
+            menu-example
+          ]) ++ (with pkgs; [
             dialog
             jq
             nixpkgs-fmt
@@ -19,18 +31,31 @@
             shellcheck
             shfmt
             treefmt
-          ]) ++ [
-            (pkgs.writeShellScriptBin "menu" ''
-              export PATH=${with pkgs; lib.makeBinPath [ dialog jq gnused ]}:"$PATH"
-              exec ${pkgs.runtimeShell} ${./menu.sh}
-            '')
-          ];
+          ]);
           shellHook = ''
             echo
-            echo -e "  Welcome! Type ‘\033[1mmenu\033[0m’ to enter a menu."
+            echo -e "  Welcome! Type ‘\033[1mmenu-example\033[0m’ to enter a menu."
+            echo
+            echo -e "  It’s the same as ‘\033[1mmenu example.json\033[0m’, if you want to play with different JSONs."
+            echo
+            echo -e "  You can also run ‘\033[1mnix run github:michalrus/nix-menu#menu-example\033[0m’ from anywhere."
             echo
           '';
         };
       });
+
+      lib = {
+        mkMenu = { pkgs, name ? "menu", definition }:
+          pkgs.writeShellScriptBin name ''
+            exec ${pkgs.lib.getExe (inputs.self.lib.mkPackage { inherit pkgs; })} \
+              ${builtins.toFile "menu.json" (builtins.toJSON definition)}
+          '';
+
+        mkPackage = { pkgs }:
+          pkgs.writeShellScriptBin "menu" ''
+            export PATH=${with pkgs; lib.makeBinPath [ dialog jq gnused ]}:"$PATH"
+            ${builtins.replaceStrings ["%NIX_MENU_VERSION%"] [(inputs.self.rev or "dirty")] (builtins.readFile ./menu.sh)}
+          '';
+      };
     };
 }
--- menu.sh
+++ menu.sh
@@ -2,9 +2,38 @@
 
 set -uo pipefail
 
-# TODO: check for: dialog/whiptail, jq, sed
+command -v sed >/dev/null 2>&1 || {
+    echo >&2 'sed: command not found'
+    exit 127
+}
+command -v jq >/dev/null 2>&1 || {
+    echo >&2 'jq: command not found'
+    exit 127
+}
+
+BACKTITLE_OPTS=("--backtitle" "nix-menu (%NIX_MENU_VERSION%)")
+
+if command -v dialog >/dev/null 2>&1; then
+    DIALOG_CMD=dialog
+    DIALOG_OPTS=(--colors --keep-tite --scrollbar --no-mouse --ok-label "Select" --cancel-label "Back" "${BACKTITLE_OPTS[@]}")
+    FMT_BOLD=$(printf "\Zb\Z7")
+    FMT_RESET=$(printf "\Zn")
+elif command -v whiptail >/dev/null 2>&1; then
+    DIALOG_CMD=whiptail
+    DIALOG_OPTS=(--ok-button "Select" --cancel-button "Back" "${BACKTITLE_OPTS[@]}")
+    FMT_BOLD=
+    FMT_RESET=
+else
+    echo >&2 'dialog: command not found'
+    echo >&2 'whiptail: command not found'
+    exit 127
+fi
 
 # TODO: check that "$1" is set or fail with usage
+if [ "$#" -ne 1 ] || ! head -c1 "$1" >/dev/null; then
+    echo "Usage: $(basename "$0") JSON_FILE"
+    exit 1
+fi
 
 draw_menu() {
     local menu_json menu_items selected exitcode new_menu_json the_command escaped_command
@@ -28,10 +57,8 @@ draw_menu() {
     set +e
 
     exec 3>&1
-    selected=$(dialog 2>&1 1>&3 \
-        --backtitle "nix-menu %NIX_MENU_VERSION%" \
-        --colors --keep-tite --scrollbar --no-mouse \
-        --ok-label "Select" --cancel-label "Back" \
+    selected=$($DIALOG_CMD 2>&1 1>&3 \
+        "${DIALOG_OPTS[@]}" \
         --title "$(jq -r '.title' <<<"$menu_json")" \
         --menu "$(jq -r 'if .longDescription == null or .longDescription == "" then .description else .longDescription end' <<<"$menu_json")" 0 0 0 \
         "${menu_items[@]}")
@@ -55,11 +82,10 @@ draw_menu() {
             escaped_command=$(/usr/bin/env bash -c 'set -x && echo "$@"' -- "${the_command[@]}" 2>&1 1>/dev/null | tail -c +8)
             set +e
 
-            if dialog \
-                --backtitle "nix-menu %NIX_MENU_VERSION%" \
-                --colors --keep-tite --scrollbar --no-mouse \
+            if $DIALOG_CMD \
+                "${DIALOG_OPTS[@]}" \
                 --title "$(jq -r '.title' <<<"$new_menu_json")" \
-                --yesno "$(printf "%s\n\n\Zb\Z7❯ %s\Zn\n\nAre you sure you want to run this command?" \
+                --yesno "$(printf "%s\n\n${FMT_BOLD}❯ %s${FMT_RESET}\n\nAre you sure you want to run this command?" \
                     "$(jq -r 'if .longDescription == null or .longDescription == "" then .description else .longDescription end' <<<"$new_menu_json")" \
                     "$escaped_command")" \
                 0 0; then
@@ -74,4 +100,4 @@ draw_menu() {
     fi
 }
 
-draw_menu <example.json
+draw_menu <"$1"
